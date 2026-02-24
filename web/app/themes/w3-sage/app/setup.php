01edfbb24d59850d7fcd961b46833c7302e2747a
gaelgerard <gael.gerard@free.fr>
2026-02-24 15:17:50 +0100

    fix; check if woocommerce installed

--- web/app/themes/w3-sage/app/setup.php
+++ web/app/themes/w3-sage/app/setup.php
@@ -196,13 +196,15 @@ add_filter('should_load_separate_core_block_assets', '__return_true');
  * Woocommerce styles only for woocommerce
  */
 add_action('wp_enqueue_scripts', function () {
-    if (!is_woocommerce() && !is_cart() && !is_checkout()) {
-        wp_dequeue_style('woocommerce-layout');
-        wp_dequeue_style('woocommerce-general');
-        wp_dequeue_style('woocommerce-smallscreen');
-        wp_dequeue_style('wc-blocks-style');
-        wp_dequeue_script('wc-add-to-cart');
-        // Ajoutez d'autres handles vus dans PageSpeed
+    if ( function_exists('is_woocommerce') ) {
+        if (!is_woocommerce() && !is_cart() && !is_checkout()) {
+            wp_dequeue_style('woocommerce-layout');
+            wp_dequeue_style('woocommerce-general');
+            wp_dequeue_style('woocommerce-smallscreen');
+            wp_dequeue_style('wc-blocks-style');
+            wp_dequeue_script('wc-add-to-cart');
+            // Ajoutez d'autres handles vus dans PageSpeed
+        }
     }
 }, 99);
 /**
